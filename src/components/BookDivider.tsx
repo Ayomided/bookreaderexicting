4fc9a50464017476b9b005b4472ce1898917b380
Ayomided <adediji.d@yahoo.com>
2025-03-27 20:33:42 +0000

    the config for markdoc is need to be present to make custom tags work

--- src/components/BookDivider.tsx
+++ src/components/BookDivider.tsx
@@ -1,10 +1,11 @@
-// components/BookDivider.ts
-
 import { fields } from "@keystatic/core";
 import { block } from "@keystatic/core/content-components";
+import { DivideSquare } from "lucide-react";
 
 export const BookDivider = block({
   label: "Book Divider",
+  icon: <DivideSquare />,
+  description: "Creates a book divider",
   schema: {
     style: fields.select({
       label: "Divider Style",
